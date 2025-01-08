077c148ce5c9ae295eef53f61a322e7fe4804f5e
N-Hosana <hosanah08@gmail.com>
2025-01-08 11:40:35 +0200

    Update urls.py

--- event/event/urls.py
+++ event/event/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from .views import EventListCreateView
+
+urlpatterns = [
+    path('api/events/', EventListCreateView.as_view(), name='event-list-create'),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
