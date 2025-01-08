a9079bede18ff5a6cbaf9c656e44ac23d53f493c
N-Hosana <hosanah08@gmail.com>
2025-01-08 11:38:26 +0200

    Update models.py

--- event/events/models.py
+++ event/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    event_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
