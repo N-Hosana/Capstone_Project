d3f06bfd0cb35720f37f97315a6c415a8ad28c5b
N-Hosana <hosanah08@gmail.com>
2025-01-09 23:01:05 +0200

    Update serializers.py

--- event/events/serializers.py
+++ event/events/serializers.py
@@ -3,5 +3,7 @@ from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
+    
         model = Event
+        fields = '__all__'
         fields = ['id', 'title', 'description', 'event_date', 'created_at', 'created_by']
