97d3a62bf152c45e5b9bdf8e60be03e88ebea9f9
N-Hosana <hosanah08@gmail.com>
2025-01-10 01:32:42 +0200

    Update models.py

--- event/events/models.py
+++ event/events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from events.models import Event
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
