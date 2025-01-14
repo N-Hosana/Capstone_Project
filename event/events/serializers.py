9bb60b7b00a5c9d578e2e26f2ea7edefc95e8ea9
N-Hosana <hosanah08@gmail.com>
2025-01-14 19:13:04 +0200

    Delete event directory

--- event/events/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Event
-
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-    
-        model = Event
-        fields = '__all__'
-        fields = ['id', 'title', 'description', 'event_date', 'created_at', 'created_by']
